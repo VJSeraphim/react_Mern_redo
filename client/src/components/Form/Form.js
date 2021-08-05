a4ef574b5ab75bdc40d106f1d89a4167e76cf094
VJS <minsung_96@naver.com>
2021-08-05 23:59:43 +0900

    Update Posts

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TextField, Typography, Paper, Button} from '@material-ui/core'
 import FileBase from 'react-file-base64'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { createPost } from '../../actions/posts'
+import { createPost, updatePost } from '../../actions/posts'
 import useStyles from './styles'
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -14,12 +14,23 @@ const Form = () => {
         tags: '',
         selectedFile: ''
     })
+
     const classes = useStyles()
     const dispatch = useDispatch()
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
+
+    useEffect(() => {
+        if(post) setPostData(post)
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        dispatch(createPost(postData))
+
+        if (currentId) {
+            dispatch(updatePost(currentId, postData))
+        } else {
+            dispatch(createPost(postData))
+        }
     }
 
     const clear = () => {
