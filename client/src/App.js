18bd15a38286e783e4f9e1ca0911dd70db1aceac
VJS <minsung_96@naver.com>
2021-08-10 23:58:58 +0900

    Auth Ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import { useDispatch } from 'react-redux'
+import React from 'react'
+import { Container } from '@material-ui/core'
 
-import memories from './images/memories.jpg'
-import Form from './components/Form/Form'
-import Posts from './components/Posts/Posts'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
-import { getPosts } from './actions/posts'
-
-import useStyles from './styles'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
 
 const App = () => {
-    const classes = useStyles()
-    const dispatch = useDispatch()
-    const [currentId, setCurrentId] = useState(null)
 
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [dispatch])
-
-    return (
+    <BrowseRouter>
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit" className={classes.appBar}>
-                <Typography variant="h2" align= "center" className={classes.heading}>
-                    Memories
-                </Typography>
-                <img src={memories} alt="memories" height="60" className={classes.image}/>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/auth" exact component={Auth}/>
+            </Switch>
+            <Home />
         </Container>
-    )
+    </BrowseRouter>
+    
+    
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
+dotenv.config()
 
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
@@ -13,10 +14,9 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://VJSeraphim:immortal2050@cluster0.xhcni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server is running on port No. ${PORT}`)))
 .catch((error) => console.log(error.message))
 
