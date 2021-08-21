1c42162cc8f8b9ae789c504687e7648afa81b633
VJS <minsung_96@naver.com>
2021-08-22 00:41:14 +0900

    Server Closed

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -8,7 +8,6 @@ import useStyles from './styles'
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
-        creator: '',
         title: '',
         message:'',
         tags: '',
@@ -18,6 +17,7 @@ const Form = ({ currentId, setCurrentId }) => {
     const classes = useStyles()
     const dispatch = useDispatch()
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
+    const user = JSON.parse(localStorage.getItem('profile'))
 
     useEffect(() => {
         if(post) setPostData(post)
@@ -26,15 +26,28 @@ const Form = ({ currentId, setCurrentId }) => {
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        if (currentId) {
-            dispatch(updatePost(currentId, postData))
+        if (currentId !== 0) {
+            dispatch(updatePost(currentId, { ...postData, name: user?.result?.name}))
+            clear()
         } else {
-            dispatch(createPost(postData))
+            dispatch(createPost({ ...postData, name: user?.result?.name}))
+            clear()
         }
     }
 
     const clear = () => {
+        setCurrentId(0)
+        setPostData({ title: '', message: '', tags: '', selectedFile: ''})
+    }
 
+    if (!user?.result?.name) {
+        return (
+            <Paper className={classes.paper}>
+                <Typography variant="h6" align="center">
+                    Please Sign in.
+                </Typography>
+            </Paper>
+        )
     }
 
     return (
@@ -43,7 +56,6 @@ const Form = ({ currentId, setCurrentId }) => {
                 <Typography variant="h6">
                     Create your memory.
                 </Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(', ') })}/>
--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use(cors())
 app.use('/posts', postRoutes)
 app.use('/user', userRoutes)
 
-
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
